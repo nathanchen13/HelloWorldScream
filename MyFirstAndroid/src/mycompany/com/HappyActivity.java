3e88783b4c570ae1bb6c325fbc851f606939d07e
nathanchen13 <nathanchen13@gmail.com>
2016-04-02 19:30:55 -0400

    update2222

--- MyFirstAndroid/src/mycompany/com/HappyActivity.java
+++ MyFirstAndroid/src/mycompany/com/HappyActivity.java
@@ -4,6 +4,7 @@
 package mycompany.com;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 
@@ -12,22 +13,30 @@ import android.view.View;
  *
  */
 public class HappyActivity extends Activity {
-	 	@Override
-	    protected void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.activity_happy);
-	    }
+	MediaPlayer mediaPlayer;
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_happy);
+    }
 
-	    public void ns_happy(View view) {
+    public void ns_happy(View view) {
+    	mediaPlayer = MediaPlayer.create(this, R.raw.nathan_super_happy);
+    	mediaPlayer.start();
+    }
 
-	    }
+    public void robert_happy(View view) {
+    	mediaPlayer = MediaPlayer.create(this, R.raw.nathan_super_happy);
+    	mediaPlayer.start();
+    }
 
-	    public void robert_happy(View view) {
-	    }
+    public void ryan_happy(View view) {
+    	mediaPlayer = MediaPlayer.create(this, R.raw.nathan_super_happy);
+    	mediaPlayer.start();
+    }
 
-	    public void ryan_happy(View view) {
-	    }
-
-	    public void n_happy(View view) {
-	    }
+    public void n_happy(View view) {
+    	mediaPlayer = MediaPlayer.create(this, R.raw.nathan_super_happy);
+    	mediaPlayer.start();
+    }
 }
