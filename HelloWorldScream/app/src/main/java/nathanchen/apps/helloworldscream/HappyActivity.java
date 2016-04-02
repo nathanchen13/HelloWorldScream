c84699d77fe1974294b4f39f3c292bdfe82c6b96
nathanchen13 <nathanchen13@gmail.com>
2016-04-02 17:10:23 -0400

    f

---
+++ HelloWorldScream/app/src/main/java/nathanchen/apps/helloworldscream/HappyActivity.java
@@ -0,0 +1,27 @@
+package nathanchen.apps.helloworldscream;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class HappyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_happy);
+    }
+
+    public void ns_happy(View view) {
+
+    }
+
+    public void robert_happy(View view) {
+    }
+
+    public void ryan_happy(View view) {
+    }
+
+    public void n_happy(View view) {
+    }
+}
